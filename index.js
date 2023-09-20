cc02604de4f8ddbf2fce3ab210a6eea7181f6abd
Austin Oblow <austinoblow97@gmail.com>
2023-09-20 14:13:06 -0500

    created a function to append the cats to the page, used a forEach to render the cats

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ document.addEventListener('DOMContentLoaded', function() {
 function fetchCats() {
     fetch('http://localhost:3000/cats') 
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => data.forEach(renderCats))
 }
+//render each cat and append it to the page
+function renderCats(cat) {
+    const catCard = document.getElementById('cat-card')
 
+    const img = document.createElement('img')
+    img.src = cat.image
+
+    catCard.append(img)
+}
